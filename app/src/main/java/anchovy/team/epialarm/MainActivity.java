e9b111309a3ecc7f2e7f1fb572082511e2dd5c6d
Str4ch <dog.fek54@gmail.com>
2025-03-27 00:54:14 +0100

    codestyle correction

--- app/src/main/java/anchovy/team/epialarm/MainActivity.java
+++ app/src/main/java/anchovy/team/epialarm/MainActivity.java
@@ -1,7 +1,6 @@
 package anchovy.team.epialarm;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
--- app/src/test/java/anchovy/team/epialarm/ExampleUnitTest.java
+++ app/src/test/java/anchovy/team/epialarm/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package anchovy.team.epialarm;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
+    public void additionIsCorrect() {
         assertEquals(4, 2 + 2);
     }
 }
